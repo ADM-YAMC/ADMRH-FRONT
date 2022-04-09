63be9dbf2b745b592a9ecbc4de68d68f6cd622fb
Jacsel Guridi <jacselguridi.dev@hotmail.com>
2022-04-09 15:53:45 -0400

    Adding gitignore

--- ADMRH/obj/Debug/net5.0/RazorDeclaration/Pages/Loadings/Loading.razor.g.cs
+++ ADMRH/obj/Debug/net5.0/RazorDeclaration/Pages/Loadings/Loading.razor.g.cs
@@ -13,112 +13,112 @@ namespace ADMRH.Pages.Loadings
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\yunior.moreta.G4S\source\repos\ADMRH-FRONT\ADMRH\_Imports.razor"
+#line 1 "C:\Users\Jacsel Guridi Diaz\Desktop\Projects\ADMRH-FRONT\ADMRH\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\yunior.moreta.G4S\source\repos\ADMRH-FRONT\ADMRH\_Imports.razor"
+#line 2 "C:\Users\Jacsel Guridi Diaz\Desktop\Projects\ADMRH-FRONT\ADMRH\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\yunior.moreta.G4S\source\repos\ADMRH-FRONT\ADMRH\_Imports.razor"
+#line 3 "C:\Users\Jacsel Guridi Diaz\Desktop\Projects\ADMRH-FRONT\ADMRH\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\yunior.moreta.G4S\source\repos\ADMRH-FRONT\ADMRH\_Imports.razor"
+#line 4 "C:\Users\Jacsel Guridi Diaz\Desktop\Projects\ADMRH-FRONT\ADMRH\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\yunior.moreta.G4S\source\repos\ADMRH-FRONT\ADMRH\_Imports.razor"
+#line 5 "C:\Users\Jacsel Guridi Diaz\Desktop\Projects\ADMRH-FRONT\ADMRH\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\yunior.moreta.G4S\source\repos\ADMRH-FRONT\ADMRH\_Imports.razor"
+#line 6 "C:\Users\Jacsel Guridi Diaz\Desktop\Projects\ADMRH-FRONT\ADMRH\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\yunior.moreta.G4S\source\repos\ADMRH-FRONT\ADMRH\_Imports.razor"
+#line 7 "C:\Users\Jacsel Guridi Diaz\Desktop\Projects\ADMRH-FRONT\ADMRH\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\yunior.moreta.G4S\source\repos\ADMRH-FRONT\ADMRH\_Imports.razor"
+#line 8 "C:\Users\Jacsel Guridi Diaz\Desktop\Projects\ADMRH-FRONT\ADMRH\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\yunior.moreta.G4S\source\repos\ADMRH-FRONT\ADMRH\_Imports.razor"
+#line 9 "C:\Users\Jacsel Guridi Diaz\Desktop\Projects\ADMRH-FRONT\ADMRH\_Imports.razor"
 using ADMRH;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\yunior.moreta.G4S\source\repos\ADMRH-FRONT\ADMRH\_Imports.razor"
+#line 10 "C:\Users\Jacsel Guridi Diaz\Desktop\Projects\ADMRH-FRONT\ADMRH\_Imports.razor"
 using ADMRH.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\yunior.moreta.G4S\source\repos\ADMRH-FRONT\ADMRH\_Imports.razor"
+#line 11 "C:\Users\Jacsel Guridi Diaz\Desktop\Projects\ADMRH-FRONT\ADMRH\_Imports.razor"
 using ADMRH.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\yunior.moreta.G4S\source\repos\ADMRH-FRONT\ADMRH\_Imports.razor"
+#line 12 "C:\Users\Jacsel Guridi Diaz\Desktop\Projects\ADMRH-FRONT\ADMRH\_Imports.razor"
 using Radzen;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "C:\Users\yunior.moreta.G4S\source\repos\ADMRH-FRONT\ADMRH\_Imports.razor"
+#line 13 "C:\Users\Jacsel Guridi Diaz\Desktop\Projects\ADMRH-FRONT\ADMRH\_Imports.razor"
 using ADMRH_API.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "C:\Users\yunior.moreta.G4S\source\repos\ADMRH-FRONT\ADMRH\_Imports.razor"
+#line 14 "C:\Users\Jacsel Guridi Diaz\Desktop\Projects\ADMRH-FRONT\ADMRH\_Imports.razor"
 using Radzen.Blazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 15 "C:\Users\yunior.moreta.G4S\source\repos\ADMRH-FRONT\ADMRH\_Imports.razor"
+#line 15 "C:\Users\Jacsel Guridi Diaz\Desktop\Projects\ADMRH-FRONT\ADMRH\_Imports.razor"
 using CurrieTechnologies.Razor.SweetAlert2;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 16 "C:\Users\yunior.moreta.G4S\source\repos\ADMRH-FRONT\ADMRH\_Imports.razor"
+#line 16 "C:\Users\Jacsel Guridi Diaz\Desktop\Projects\ADMRH-FRONT\ADMRH\_Imports.razor"
 using ADMRH.Pages.Loadings;
 
 #line default
